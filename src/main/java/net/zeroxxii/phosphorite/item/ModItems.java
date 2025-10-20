a6bfeca422367b8b6b329d657f9bb51f129c5b76
ZeroXX11 <classic.evolved.official@gmail.com>
2025-10-20 15:16:15 +0200

    Removed automatic item addition to the Ingredients tab

--- src/main/java/net/zeroxxii/phosphorite/item/ModItems.java
+++ src/main/java/net/zeroxxii/phosphorite/item/ModItems.java
@@ -1,10 +1,7 @@
 package net.zeroxxii.phosphorite.item;
 
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
-import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroupEntries;
-import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.item.Item;
-import net.minecraft.item.ItemGroups;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
@@ -14,18 +11,11 @@ public class ModItems {
     public static final Item RAW_PHOSPHORITE = registerItem("raw_phosphorite", new Item(new FabricItemSettings()));
     public static final Item PHOSPHORITE = registerItem("phosphorite", new Item(new FabricItemSettings()));
 
-    private static void addItemsToIngredientItemGroup(FabricItemGroupEntries entries) {
-        entries.add(RAW_PHOSPHORITE);
-        entries.add(PHOSPHORITE);
-    }
-
     private static Item registerItem(String name, Item item) {
         return Registry.register(Registries.ITEM, new Identifier(Phosphorite.MOD_ID, name), item);
     }
 
     public static void registerModItems() {
         Phosphorite.LOGGER.info("Registering Mod Items for " + Phosphorite.MOD_ID);
-
-        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(ModItems::addItemsToIngredientItemGroup);
     }
 }
